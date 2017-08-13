93317afed1f7b44cb48fefe2605648d1af002499
Joe Johans <lgvichy@gmail.com>
2017-08-13 15:49:14 -0700

    Use a different adapter for Loki

--- app/components/Counter/model.js
+++ app/components/Counter/model.js
@@ -1,13 +1,20 @@
 import {action, reaction, observable, observe, computed, autorun} from 'mobx';
 import autobind from 'autobind-decorator'
 
+import BLEMonitor from './BLEMonitor';
+
 @autobind
 class Model {
   @observable counter = 0;
   @observable total = 0;
 
+	bleMonitor = BLEMonitor()
+
   constructor(){
     reaction(()=>this.counter, this.increaseTotal);
+
+		bleMonitor.start()
+
   }
 
   @action increaseTotal(){
